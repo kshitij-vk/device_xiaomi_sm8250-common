f7c209a1a6c9f6748a3df443d8596369294933ac
LuK1337 <priv.luk@gmail.com>
2025-05-17 13:07:24 +0000

    sm8350-common: Add libwfdservice_shim_v1 for WFD e69a5be2449122f90220151f9d01ace15221facf in system/hardware/interfaces broke the ABI ;'(
    Change-Id: Id993d3c40aaa8e52fbfe404d174293ce2696fc04

--- extract-files.py
+++ extract-files.py
@@ -25,6 +25,8 @@ blob_fixups: blob_fixups_user_type = {
         .add_needed('libinput_shim.so'),
     'system_ext/lib64/libwfdservice.so': blob_fixup()
         .replace_needed('android.media.audio.common.types-V2-cpp.so', 'android.media.audio.common.types-V4-cpp.so'),
+    'system_ext/bin/wfdservice64': blob_fixup()
+        .add_needed('libwfdservice_shim_v1.so'),
     'vendor/etc/init/init.mi_thermald.rc': blob_fixup()
         .regex_replace('.*seclabel u:r:mi_thermald:s0\n', ''),
     'vendor/etc/seccomp_policy/atfwd@2.0.policy': blob_fixup()
