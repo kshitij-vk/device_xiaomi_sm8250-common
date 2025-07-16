c129512304726bb58a789477b8067a0a80432de7
Michael Bestas <mkbestas@lineageos.org>
2025-07-16 08:01:40 +0000

    sm8250-common: Move Lineage Health HAL to select() - Its deprecated
    vendor/lineage/config/BoardConfigSoong.mk:66: warning: TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_BYPASS is deprecated, please migrate to soong_config_set,lineage_health,charging_control_supports_bypass
    
    Change-Id: Ie82a8aa7d1766e67b83eef8066d4dcf177e04629
    Signed-off-by: Sanjivns <113604406+Sanjivns@users.noreply.github.com>

--- kona.mk
+++ kona.mk
@@ -277,6 +277,8 @@ PRODUCT_PACKAGES += \
      vendor.qti.hardware.display.composer-service \
      vendor.xiaomi.sensor.citsensorservice-service.aidl
 
+$(call soong_config_set,lineage_health,charging_control_supports_bypass,false)
+
  ifneq ($(TARGET_BUILD_VARIANT),eng)
  PRODUCT_VENDOR_PROPERTIES += \
      $(foreach tag,$(SPAMMY_LOG_TAGS),log.tag.$(tag)=E)
