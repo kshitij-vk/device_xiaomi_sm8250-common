70baa158e0f70ad54f2e10affd7dbaefaf6db777
Kshitij Koyande <68855313+kshitij-vk@users.noreply.github.com>
2025-08-11 07:25:36 +0000

    sm8250-common: Build Xiaomi touch This reverts commit 1cb9f62.
    Change-Id: I6ed8f876413047bc8b68896fe8c32b497295be5c
    Signed-off-by: Kshitij Koyande <68855313+kshitij-vk@users.noreply.github.com>

--- kona.mk
+++ kona.mk
@@ -421,6 +421,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
+# Touch
+PRODUCT_PACKAGES += \
+    vendor.lineage.touch@1.0-service.xiaomi
+
 # Thermal
 ifeq ($(TARGET_USE_QTI_THERMAL_SERVICE),true)
 PRODUCT_PACKAGES += \
