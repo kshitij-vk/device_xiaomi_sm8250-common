be4e83fa701632514b90ac44b881be6d9fc8bdd4
darkphnx <dark.phnx12@gmail.com>
2025-08-19 15:04:48 +0000

    F

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -64,7 +64,7 @@ TARGET_NO_BOOTLOADER := true
 MALLOC_SVELTE_FOR_LIBC32 := true
 
 # Camera - Miui
-TARGET_CAMERA_PACKAGE_NAME := com.android.camera
+#TARGET_CAMERA_PACKAGE_NAME := com.android.camera
 
 # Camera - HIDL overrideFormat
 TARGET_CAMERA_USES_NEWER_HIDL_OVERRIDE_FORMAT = true
