c16f592bdf4639c0c6dc62f9c66871515bd2a3cf
Kshitij Koyande <68855313+kshitij-vk@users.noreply.github.com>
2025-11-21 20:40:57 +0530

    F
    Change-Id: Ic837f4dcefc329e916bf8510d7fdbe943b9f82f3

--- kona.mk
+++ kona.mk
@@ -491,8 +491,7 @@ PRODUCT_PACKAGES += \
 # Wi-Fi Display
 PRODUCT_PACKAGES += \
     libinput_shim \
-    libgui_shim \
-    libwfdaac_vendor
+    libgui_shim
 
 PRODUCT_BOOT_JARS += \
     WfdCommon
